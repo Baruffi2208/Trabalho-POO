be80ce3360bb1b63f7fb3c5c0687ca814e7e9f96
Baruffi2208 <GBPastorio@ucs.br>
2025-05-19 11:20:01 -0300

    Create Produto.java

---
+++ TrabalhoJava/Loja/src/loja/Produto.java
@@ -0,0 +1,64 @@
+package loja;
+
+public class Produto {
+    private static int contadorId = 0;
+
+    private int id;
+    private String nome;
+    private String descricao;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, String descricao, double preco, int quantidade) {
+        this.id = contadorId++;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+               "\nNome: " + nome +
+               "\nDescrição: " + descricao +
+               "\nPreço: R$ " + preco +
+               "\nQuantidade: " + quantidade;
+    }
+}
